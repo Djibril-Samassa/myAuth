26afd816361d4d42db3455fae7868342846b03d9
Djibril <djibrilsamassa.pro@gmail.com>
2023-03-05 19:51:30 +0100

    style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
 import jwt_decode from "jwt-decode";
 import { useState, useEffect } from 'react';
+import { Icon } from '@iconify/react';
 import { supabase } from './client';
 import axios, { Axios } from 'axios';
 
@@ -11,6 +12,7 @@ function App() {
   const [user, setUser] = useState(null)
   const [repoCount, setRepoCount] = useState()
   const [link, setLink] = useState()
+  const [providerName, setProviderName] = useState('')
   useEffect(() => {
     window.addEventListener('hashchange', function () {
       checkUser();
@@ -22,16 +24,19 @@ function App() {
   }, [user])
 
   const count = async () => {
-    const response = await fetch(`https://api.github.com/users/${user.user_name}/repos`)
+    const response = await fetch(`https://api.github.com/users/${user?.user_name}/repos`)
     const data = await response.json()
     setRepoCount(data)
-    setLink(`https://github.com/${user.user_name}?tab=repositories`)
-    console.log(data)
+    setLink(`https://github.com/${user?.user_name}?tab=repositories`)
   }
+
 
   async function checkUser() {
     supabase.auth.getUser()
-      .then((res) => { setUser(res.data.user.user_metadata) })
+      .then((res) => {
+        setProviderName(res.data.user.app_metadata.provider)
+        setUser(res.data.user.user_metadata)
+      })
       .catch((err) => { console.log(err) })
   }
 
@@ -41,9 +46,18 @@ function App() {
     })
   }
 
+  async function googleSign() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    })
+  }
+
+
+
   async function signOut() {
     await supabase.auth.signOut;
     setUser(null)
+    setProviderName('')
   }
 
 
@@ -53,38 +67,46 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        {user ?<>
-          <div className='data'>
-            <img className='profpic' src={user.avatar_url} />
-            <h3>Bienvenue {user.name} vous êtes connecté avec votre compte Github</h3>
-            <p>Adresse e-mail: {user.email}</p>
-            <p>Nombre de repositories public sur Github : {repoCount?.length}  </p>
-            <span className='repolist'>{repoCount?.slice(0, 10).map((repo) => {
-              return <li>{repo.name} /</li>
-            })}...et plus</span>
-          </div>
-            <a className='link' href={link} target="_blank">Voir plus</a></>
+        {!user ?
+          <>
+            <h3>Bienvenue sur MyAuth par Djibril SAMASSA</h3>
+            <p>Conncetez vous avec vos comptes d'autres plateforme</p>
+          </>
           : null}
-        {user === null ? <button onClick={() => { githubSign() }}>Se connecter avec Github</button> : <button onClick={() => { signOut() }}>Se déconnecter</button>}
-        {/* {user === undefined ?
-          <GoogleLogin
-            onSuccess={credentialResponse => {
-              console.log(credentialResponse);
-              const decoded = jwt_decode(credentialResponse.credential)
-              setUser(decoded)
-            }}
-            onError={() => {
-              console.log('Login Failed');
-            }}
-          /> :
-          <div>
-            <h3>Bonjour {user.name}</h3>
-            <img src={user.picture} />
-            <p>Vous êtes connecté avec le compte: <br /> {user.email}</p>
-            <button onClick={() => { setUser() }}>Se deconnecter</button>
+        {console.log(user)}
+
+        {user ?
+          // CONNECTE AVEC GITHUB
+          providerName === "github" ?
+            <>
+              <div className='data'>
+                <img className='profpic' src={user.avatar_url} />
+                <h3>Bienvenue {user.name} vous êtes connecté avec votre compte Github</h3>
+                <p>Adresse e-mail: {user.email}</p>
+                <p>Nombre de repositories public sur Github : {repoCount?.length}  </p>
+                <span className='repolist'>{repoCount?.slice(0, 10).map((repo) => {
+                  return <li>{repo.name} /</li>
+                })}...et plus</span>
+              </div>
+              <a className='link' href={link} target="_blank">Voir plus</a>
+            </>
+            // CONNECTE AVEC GOOGLE 
+            : providerName === "google" ?
+              <>
+                <div className='data'>
+                  <img className='profpic' src={user.avatar_url} />
+                  <h3>Bienvenue {user.name} vous êtes connecté avec votre compte Google</h3>
+                  <p>Adresse e-mail: {user.email}</p>
+                </div>
+              </>
+              : null
+          : null}
+        {user === null ?
+          <div className='buttonC'>
+            <button className='login' onClick={() => { githubSign() }}><Icon className='icon' icon="akar-icons:github-fill" width="30" height="30" /> Se connecter avec Github</button>
+            <button className='login' onClick={() => { googleSign() }}><Icon className='icon' icon="ion:logo-google" width="30" height="30" />Se connecter avec Google</button>
           </div>
-        } */}
+          : <button className='logout' onClick={() => { signOut() }}><Icon className='icon' icon="ic:baseline-log-out" width="30" height="30" />Se déconnecter</button>}
       </header>
     </div>
   );
--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://ywwsbowtxuzeguobzoqz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3d3Nib3d0eHV6ZWd1b2J6b3F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgwMjk4NjUsImV4cCI6MTk5MzYwNTg2NX0.N62KWbmiGETlQLHCHC83fq63t5SY_yulyHyKZTfT0W0')
 
+
 export {
     supabase
 }
